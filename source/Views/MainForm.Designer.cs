3c03c460346264564cbc0b8e69a03efa835c3197
巽翎君 <cnzhnet@hotmail.com>
2021-02-26 02:23:27 +0800

    Add files via upload

---
+++ source/Views/MainForm.Designer.cs
@@ -0,0 +1,273 @@
+﻿#if platform_windows
+namespace cnzhnet.music_decrypt.Views
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.outputDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bottomPanel.SuspendLayout();
+            this.contentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Controls.Add(this.label3);
+            this.bottomPanel.Controls.Add(this.btnDecrypt);
+            this.bottomPanel.Controls.Add(this.btnRemove);
+            this.bottomPanel.Controls.Add(this.btnAddFile);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 352);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(620, 56);
+            this.bottomPanel.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(218)))));
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(620, 1);
+            this.label3.TabIndex = 3;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecrypt.Location = new System.Drawing.Point(521, 15);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(89, 25);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "解密";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(426, 15);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(89, 25);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "移除文件";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddFile.Location = new System.Drawing.Point(331, 15);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(89, 25);
+            this.btnAddFile.TabIndex = 0;
+            this.btnAddFile.Text = "添加文件";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.BackColor = System.Drawing.Color.White;
+            this.contentPanel.Controls.Add(this.btnBrowser);
+            this.contentPanel.Controls.Add(this.outputDir);
+            this.contentPanel.Controls.Add(this.label2);
+            this.contentPanel.Controls.Add(this.dataGridView1);
+            this.contentPanel.Controls.Add(this.label1);
+            this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentPanel.Location = new System.Drawing.Point(0, 0);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(620, 352);
+            this.contentPanel.TabIndex = 1;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowser.Location = new System.Drawing.Point(535, 305);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 25);
+            this.btnBrowser.TabIndex = 4;
+            this.btnBrowser.Text = "浏览(&B)";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // outputDir
+            // 
+            this.outputDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputDir.Location = new System.Drawing.Point(86, 306);
+            this.outputDir.Name = "outputDir";
+            this.outputDir.PlaceholderText = "解密后的输出位置.";
+            this.outputDir.Size = new System.Drawing.Size(443, 23);
+            this.outputDir.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输出位置：";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnStatus,
+            this.ColumnFile,
+            this.ColumnPath});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(598, 256);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "加密的音频文件：";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "选择解密的输出目录.";
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.AddExtension = false;
+            this.openFileDialog1.Filter = "酷我加密音频|*.kwm";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "选择要解密的音频文件";
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.DataPropertyName = "Id";
+            this.ColumnId.HeaderText = "唯一";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.ReadOnly = true;
+            this.ColumnId.Visible = false;
+            // 
+            // ColumnStatus
+            // 
+            this.ColumnStatus.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ColumnStatus.DataPropertyName = "Status";
+            this.ColumnStatus.HeaderText = "状态";
+            this.ColumnStatus.Name = "ColumnStatus";
+            this.ColumnStatus.ReadOnly = true;
+            // 
+            // ColumnFile
+            // 
+            this.ColumnFile.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ColumnFile.DataPropertyName = "File";
+            this.ColumnFile.HeaderText = "文件名";
+            this.ColumnFile.Name = "ColumnFile";
+            this.ColumnFile.ReadOnly = true;
+            this.ColumnFile.Width = 240;
+            // 
+            // ColumnPath
+            // 
+            this.ColumnPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnPath.DataPropertyName = "FullPath";
+            this.ColumnPath.HeaderText = "路径";
+            this.ColumnPath.Name = "ColumnPath";
+            this.ColumnPath.ReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 408);
+            this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "酷我音乐无损加密音频破解器";
+            this.bottomPanel.ResumeLayout(false);
+            this.contentPanel.ResumeLayout(false);
+            this.contentPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel contentPanel;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox outputDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPath;
+    }
+}
+#endif
+
---
+++ source/Views/MainForm.cs
@@ -0,0 +1,217 @@
+﻿#if platform_windows
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using cnzhnet.music_decrypt.Models;
+using cnzhnet.music_decrypt.Services;
+
+namespace cnzhnet.music_decrypt.Views
+{
+    /// <summary>
+    /// 主窗口.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        private BindingList<DecryptAudioItem> audioItems;
+        private SynchronizationContext syncContext;
+        private int processed;
+
+        /// <summary>
+        /// 创建主窗口实例.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+            audioItems = new BindingList<DecryptAudioItem>();
+            dataGridView1.DataSource = audioItems;
+        }
+        /// <summary>
+        /// 主窗口加载时执行此方法.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (DesignMode)
+                return;
+            syncContext = SynchronizationContext.Current;
+            // 在此注册支持的音频流解密器.
+            StreamDecrypter.RegisterDecrypter(".kwm", typeof(KwmStreamDecrypter));
+            // ... 其它类型的音频支持扩展后在此注册.
+        }
+        /// <summary>
+        /// 添加文件.
+        /// </summary>
+        private void AppendFiles()
+        {
+            switch (openFileDialog1.ShowDialog(this))
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    foreach (string f in openFileDialog1.FileNames)
+                    {
+                        audioItems.Add(new DecryptAudioItem { 
+                            File = Path.GetFileName(f),
+                            FullPath = f
+                        });
+                    }
+                    dataGridView1.Refresh();
+                    break;
+            }
+        }
+        /// <summary>
+        /// 移除文件.
+        /// </summary>
+        private void RemoveItem()
+        {
+            if (dataGridView1.SelectedRows.Count < 1)
+                return;
+            List<DecryptAudioItem> items = new List<DecryptAudioItem>();
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                items.Add((DecryptAudioItem)(row.DataBoundItem));
+            for (int i = 0; i < items.Count; ++i)
+                audioItems.Remove(items[i]);
+            dataGridView1.Refresh();
+        }
+        /// <summary>
+        /// 选定输出目录.
+        /// </summary>
+        private void SelectOutputDir()
+        {
+            switch (folderBrowserDialog1.ShowDialog(this))
+            {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    outputDir.Text = folderBrowserDialog1.SelectedPath;
+                    break;
+            }
+        }
+        /// <summary>
+        /// 解密列表中的音频项（逐渐条递进式）.
+        /// </summary>
+        private void DecryptItem()
+        {
+            if (processed >= audioItems.Count) // 列表中的所有音频皆已处理完成时.
+            {
+                MessageBox.Show($"已处理完成 {processed} 加密个音频，处理情况见列表.", "任务完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bottomPanel.Enabled = true;
+                return;
+            }
+            DecryptAudioItem item;
+            IStreamDecrypter decrypter;
+            do  // 若所添加的加密音频未被支持则应忽略之并递进处理下一个.
+            {
+                item = audioItems[processed];
+                decrypter = StreamDecrypter.GetDecrypter(Path.GetExtension(item.File));
+                if (decrypter == null)
+                {
+                    item.Status = "不支持.";
+                    dataGridView1.InvalidateRow(processed);
+                    Interlocked.Increment(ref processed);
+                }
+            }
+            while (decrypter == null);
+            // 准备解密支持的加密音频.
+            decrypter.Completed += Decrypter_Completed;
+            decrypter.Source = File.Open(item.FullPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            item.Output = $"{Path.Combine(outputDir.Text, Path.GetFileNameWithoutExtension(item.File))}.tmp";
+            item.Status = "解密中...";
+            if (File.Exists(item.Output))
+                File.Delete(item.Output);
+            decrypter.Output = File.Open(item.Output, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            decrypter.Output.Position = 0;
+            decrypter.Decrypt(item.Id); // 解密音频.
+        }
+        /// <summary>
+        /// 解密码完成的事件处理程序.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Decrypter_Completed(IStreamDecrypter sender, CompletedEventArgs e)
+        {   // 释放为目标解密器打开的源及输出目标流.
+            sender.Source?.Close();
+            sender.Source?.Dispose();
+            sender.Source = null;
+            sender.Output?.Flush();
+            sender.Output?.Close();
+            sender.Output?.Dispose();
+            sender.Output = null;
+            // 更新当前解密项的列表中的显示状态.
+            DecryptAudioItem item = audioItems.Where(p => p.Id == e.Id).First();
+            int index = audioItems.IndexOf(item);
+            if (item != null)
+            {
+                if (e.Success)
+                {
+                    item.Status = "已解密";
+                    File.Move(item.Output, $"{Path.Combine(Path.GetDirectoryName(item.Output), Path.GetFileNameWithoutExtension(item.File))}.flac", true);
+                }
+                else
+                {
+                    item.Status = $"解密错误：{e.Error.Message}";
+                    File.Delete(item.Output);
+                }
+            }
+            syncContext.Send((state) => {
+                dataGridView1.InvalidateRow((int)state);
+                Interlocked.Increment(ref processed); // 递进到列表中的下一个音频.           
+                DecryptItem(); // 下一个音频的解密.
+            }, index);
+        }
+
+        /// <summary>
+        /// 启动解密任务.
+        /// </summary>
+        private void StartDectypt()
+        {
+            if (string.IsNullOrEmpty(outputDir.Text)) 
+            {
+                MessageBox.Show("请先选择输出目录！", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (audioItems.Count < 1) 
+            {
+                MessageBox.Show("请先添加要解密的音频文件！", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Interlocked.Exchange(ref processed, 0);
+            bottomPanel.Enabled = false;
+            DecryptItem();
+        }
+
+        /// <summary>
+        /// 按钮事件响应处理程序.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null)
+                return;
+
+            switch (btn.Name)
+            {
+                case "btnBrowser":
+                    SelectOutputDir();
+                    break;
+                case "btnAddFile":
+                    AppendFiles();
+                    break;
+                case "btnRemove":
+                    RemoveItem();
+                    break;
+                case "btnDecrypt":
+                    StartDectypt();
+                    break;
+            }
+        }
+    }
+}
+#endif
